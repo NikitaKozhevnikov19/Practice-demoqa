5a88d4f3453a83e7848520992bed706ee37665fb
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-10-12 19:59:11 +0500

    added CalendarComponent.java

---
+++ src/test/java/pages/components/CalendarComponent.java
@@ -0,0 +1,17 @@
+package pages.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class CalendarComponent {
+    private SelenideElement yearPicker = $(".react-datepicker__year-select"),
+            monthPicker = $(".react-datepicker__month-select");
+    private String dayPicker = ".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)";
+
+    public void setDate(String day, String month, String year) {
+        yearPicker.selectOption(year);
+        monthPicker.selectOption(month);
+        $(String.format(dayPicker, day)).click();
+    }
+}
